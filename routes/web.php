7606dafd77dcc87a963b723799a0da5aecd51877
Billy Roberts <billyjamesroberts@hotmail.com>
2019-12-08 15:12:11 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/yo', function () {
+    return 'yo';
+});
+
+Route::get('/animals', function () {
+    return '<head>sheep init</head>';
+});
