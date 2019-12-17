d021f0a3dd67084f6e752882c8e80fb0dbedfaa8
Billy Roberts <billyjamesroberts@hotmail.com>
2019-12-17 14:06:17 +0000

    Implemented pagination and bootstrap to Comments.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,15 +6,36 @@
 
     <h4><a href="{{ route('posts.create') }}">Want to create a post? Click here!</a></h4>
 
-    @foreach($posts as $post)
-        <h5>Post by: <a href="{{ route('profiles.show', ['profile' => $post->profile]) }}">
-            {{ $post->profile->display_name }}</a></h5>
-            
-        <p>{{ $post->text }}</p>
-        <p>Comments: {{ $post->comments->count() }} 
-        <a href="{{ route('posts.show', ['post' => $post]) }}"><input type="button" value="View Post"></a></p>
-        
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+ 
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    @endforeach
-    {!! $posts->links() !!}
+    <div class="container">
+        <table class="table table-bordered" id="laravel">
+           <thead>
+              <tr>
+                 <th>Profile</th>
+                 <th>Content</th>
+                 <th>Comments</th>
+                 <th>Date Created</th>
+                 <th>Action</th>
+              </tr>
+           </thead>
+           <tbody>
+              @foreach($posts as $post)
+
+                <tr>
+                    <td><a href="{{ route('profiles.show', ['profile' => $post->profile]) }}">
+                        {{ $post->profile-> display_name }}</a></td>
+                    <td>{{ $post->text }}</td>
+                    <td>{{ $post->comments->count() }}</td>
+                    <td>{{ date('d m Y', strtotime($post->created_at)) }}</td>
+                    <td><a href="{{ route('posts.show', ['post' => $post]) }}"><input type="button" value="View Post"></a></td>
+                </tr>
+
+              @endforeach
+           </tbody>
+        </table>
+     {!! $posts->links() !!}
+     </div>
 @endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,14 +7,37 @@
 
     <h3>{{ $post->profile->display_name }}:</h1>
     <p>{{ $post->text }}</p>
+    <a href="{{ route('comments.create', ['post' => $post]) }}"><input type="button" value="Add Comment"></a>
     <a href="{{ route('posts.edit', ['post' => $post]) }}"><input type="button" value="Edit Post"></a>
     <h3>Comments:</h2>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+ 
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    @foreach($post->comments as $comment)
+    <div class="container">
+        <table class="table table-bordered" id="laravel">
+           <thead>
+              <tr>
+                 <th>Profile</th>
+                 <th>Content</th>
+                 <th>Date Created</th>
+                 <th>Action</th>
+              </tr>
+           </thead>
+           <tbody>
+              @foreach($post->comments as $comment)
 
-    <p><a href="{{ route('profiles.show', ['profile' => $comment->profile]) }}">{{ $comment->profile-> display_name }}</a>: {{ $comment->text}}</p>
-    <a href="{{ route('comments.edit', ['comment' => $comment]) }}"><input type="button" value="Edit Comment"></a>
-    @endforeach
+                <tr>
+                    <td><a href="{{ route('profiles.show', ['profile' => $comment->profile]) }}">
+                        {{ $comment->profile->display_name }}</a></td>
+                    <td>{{ $comment->text }}</td>
+                    <td>{{ date('d m Y', strtotime($comment->created_at)) }}</td>
+                    <td><a href="{{ route('comments.edit', ['comment' => $comment]) }}"><input type="button" value="Edit Comment"></a></td>
+                </tr>
 
+              @endforeach
+           </tbody>
+        </table>
+     </div>
 
 @endsection
